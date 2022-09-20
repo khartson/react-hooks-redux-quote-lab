3ec01fa3686cbee83fa9f23a20bdb02e63401f7d
Kyle <khartson2017@gmail.com>
2022-09-20 15:51:46 -0400

    commit tree b96e0b5002ac1abba0e3063ee7c59c5148df2a7f

--- src/features/quotes/QuoteCard.js
+++ src/features/quotes/QuoteCard.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { useDispatch } from 'react-redux';
 
 function QuoteCard(props) {
+  const dispatch = useDispatch();
+  function handleUpvote() {
+    dispatch(props.upvoteQuote(props.quote.id));
+  }
+
+  function handleDownvote() {
+    dispatch(props.downvoteQuote(props.quote.id));
+  }
+
+  function handleRemove() {
+    dispatch(props.removeQuote(props.quote.id))
+  }
   return (
     <div>
       <div className="card card-inverse card-success card-primary mb-3 text-center">
         <div className="card-block">
           <blockquote className="card-blockquote">
-            <p>{/*Render Quote Content*/}</p>
+            <p>{props.quote.content}</p>
             <footer>
               - author{" "}
-              <cite title="Source Title">{/*Render Quote Author*/}</cite>
+              <cite title="Source Title">{props.quote.author}</cite>
             </footer>
           </blockquote>
         </div>
@@ -19,17 +32,17 @@ function QuoteCard(props) {
             role="group"
             aria-label="Basic example"
           >
-            <button type="button" className="btn btn-primary">
+            <button  onClick={handleUpvote}type="button" className="btn btn-primary">
               Upvote
             </button>
-            <button type="button" className="btn btn-secondary">
+            <button onClick={handleDownvote}type="button" className="btn btn-secondary">
               Downvote
             </button>
-            <button type="button" className="btn btn-danger">
+            <button onClick={handleRemove} type="button" className="btn btn-danger">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div>Votes: {/*Render Quote Votes*/}</div>
+          <div>Votes: {props.quote.votes}</div>
         </div>
       </div>
     </div>
--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,9 +1,72 @@
 // Action Creators
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+  return {
+    type: "quotes/add",
+    payload: quote,
+  };
+};
+
+export const removeQuote = (quoteId) => {
+  return {
+    type: 'quotes/remove',
+    payload: quoteId,
+  };
+};
+
+export const upvoteQuote = (quoteId) => {
+  return {
+    type: 'quotes/upvote',
+    payload: quoteId,
+  };
+};
+
+export const downvoteQuote = (quoteId) => {
+  return {
+    type: 'quotes/downvote',
+    payload: quoteId,
+  };
+};
 
 // Reducer
 const initialState = [];
 
 export default function quotesReducer(state = initialState, action) {
-  return state;
+  switch(action.type ) {
+    case 'quotes/add':
+      return [...state, action.payload];
+    case 'quotes/remove':
+      return state.filter((quote) => quote.id !== action.payload);
+    case 'quotes/upvote':
+      return state.map((item) => {
+        if (item.id !== action.payload) {
+          return item
+        }
+        if (!item.votes) {
+          return {
+            ...item,
+            votes: 1,
+          }
+        }
+        return {
+          ...item,
+          votes: item.votes + 1,
+        }
+      });
+    case 'quotes/downvote':
+      return state.map((item) => {
+        if (item.id !== action.payload) {
+          return item
+        }
+        if (item.votes > 0) {
+          return {
+            ...item,
+            votes: item.votes - 1,
+          }
+        }
+        return item;
+      });
+    default: 
+      return state;
+  }
 }
